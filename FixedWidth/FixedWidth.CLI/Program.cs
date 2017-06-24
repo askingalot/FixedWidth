cf53a252e70f37184b44af85a04e37592eca2823
Andy Collins <askingalot@gmail.com>
2017-06-24 13:32:48 -0500

    Fleshing out the low-level API. Still very early.

--- FixedWidth/FixedWidth.CLI/Program.cs
+++ FixedWidth/FixedWidth.CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -11,19 +12,73 @@ namespace FixedWidth.CLI
         {
             var dataFile = $"..{Path.DirectorySeparatorChar}data.txt";
 
+            var reader = new Reader(9, 6, 10, 10);
             var records = File.ReadAllLines(dataFile)
                 .Select(line =>
                     new Record
                     {
-                        FirstName = line.Substring(0, 9).Trim(),
-                        LastName = line.Substring(9, 6).Trim(),
-                        BirthDate = DateTime.ParseExact(line.Substring(15, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture),
-                        DeathDate = DateTime.ParseExact(line.Substring(25, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture)
+                        FirstName = reader.GetString(line, 0),
+                        LastName = reader.GetString(line, 1),
+                        BirthDate = reader.GetDatetime(line, 2, "yyyy-MM-dd"),
+                        DeathDate = reader.GetDatetime(line, 3, "yyyy-MM-dd"),
                     }
                 ).ToList();
 
             records.ForEach(Console.WriteLine);
         }
+    }
+
+
+    public class Reader
+    {
+        private FileLayout Layout;
+
+        public Reader(FileLayout layout)
+        {
+            Layout = layout;
+        }
+
+        public Reader(params int[] sizes) : this(new FileLayout(sizes)) { }
+        public Reader(IEnumerable<int> sizes) : this(new FileLayout(sizes)) { }
+
+        public string GetString(string record, int fieldIndex) =>
+            GetString(record, Layout.FieldData[fieldIndex].Start, Layout.FieldData[fieldIndex].Length);
+
+        public DateTime GetDatetime(string record, int fieldIndex, string format)
+        {
+            var str = GetString(record, fieldIndex);
+            return DateTime.ParseExact(str, format, CultureInfo.InvariantCulture);
+        }
+
+
+        private string GetString(string record, int start, int length) =>
+            record.Substring(start, length).Trim();
+
+    }
+
+    public class FileLayout
+    {
+        public (int Start, int Length)[] FieldData { get; }
+
+        public FileLayout(params int[] sizes)
+        {
+            if (sizes == null || sizes.Length == 0)
+            {
+                throw new ArgumentException(nameof(sizes), 
+                    $"{nameof(sizes)} must contain at least one element.");
+            }
+
+            FieldData = new(int, int)[sizes.Length];
+            var start = 0;
+            for(var i=0; i<sizes.Length; i++)
+            {
+                FieldData[i].Start = start;
+                FieldData[i].Length = sizes[i];
+                start += sizes[i];
+            }
+        }
+
+        public FileLayout(IEnumerable<int> sizes) : this(sizes.ToArray()) { }
     }
 
     public class Record
