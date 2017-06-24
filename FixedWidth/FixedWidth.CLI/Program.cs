ab9b5d14c8910e82ebcc7cb7ae78fc68c79a6297
Andy Collins <askingalot@gmail.com>
2017-06-24 12:50:49 -0500

    Initial solution setup.

---
+++ FixedWidth/FixedWidth.CLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace FixedWidth.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataFile = $"..{Path.DirectorySeparatorChar}data.txt";
+
+            var records = File.ReadAllLines(dataFile)
+                .Select(line =>
+                    new Record
+                    {
+                        FirstName = line.Substring(0, 9).Trim(),
+                        LastName = line.Substring(9, 6).Trim(),
+                        BirthDate = DateTime.ParseExact(line.Substring(15, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture),
+                        DeathDate = DateTime.ParseExact(line.Substring(25, 10), "yyyy-MM-dd", CultureInfo.InvariantCulture)
+                    }
+                ).ToList();
+
+            records.ForEach(Console.WriteLine);
+        }
+    }
+
+    public class Record
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime DeathDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} was born on {BirthDate.ToString("dd/MM/yyyy")} and died on {DeathDate.ToString("dd/MM/yyyy")}";
+        }
+    }
+}
---
+++ FixedWidth/FixedWidth.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FixedWidth.Core
+{
+    public class Class1
+    {
+    }
+}
